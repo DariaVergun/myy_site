287ffc02dc50d24f0e4191bec97687f8b679570f
DariaVergun <114015139+DariaVergun@users.noreply.github.com>
2024-03-14 15:37:59 +0200

    Update button.js

--- button.js
+++ button.js
@@ -1,13 +1,13 @@
-// пропишем функцию random
+
 const random = (min, max) => 
 {
     const rand = min + Math.random() * (max - min + 1);
     return Math.floor(rand);
 }
 
-// найдем кнопку
+
 const btn = document.querySelector('#btn');
-// повесим обработчик событий
+
 btn.addEventListener('mouseenter', () => 
 {
     btn.style.left = `${random(0, 90)}%`;
@@ -22,14 +22,14 @@ document.getElementById('buttonToHeart').addEventListener('click', function() {
         this.style.color = 'white';
     } else {
         this.innerText = 'ТАК)';
-        this.style.backgroundColor = ''; // Повертаємо колір за замовчуванням
-        this.style.color = ''; // Повертаємо колір тексту за замовчуванням
+        this.style.backgroundColor = ''; 
+        this.style.color = ''; 
     }
 });
 
 const h1Text = "Го гуляти??";
 
-// Знаходимо елемент заголовка за його тегом
+
 const h1Element = document.querySelector('h1');
 
 // Вставляємо текст у заголовок
